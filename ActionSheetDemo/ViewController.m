08407b66a5f2b18d9c028d858a642941d89be4f3
Anthony Dagati <tdagati.cs@gmail.com>
2014-11-14 08:50:44 -0800

    Added delete confirmation and show colors confirmation; added ActionSheet tags

--- ActionSheetDemo/ViewController.m
+++ ActionSheetDemo/ViewController.m
@@ -27,11 +27,19 @@
 - (IBAction)showNormalActionSheet:(id)sender {
     UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"What do you want to do with the file?" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:@"Delete" otherButtonTitles:@"Copy", @"Move", @"Duplicate", nil];
     [actionSheet showInView:self.view];
+    actionSheet.tag = 100;
 }
 
 - (IBAction)showDeleteConfirmation:(id)sender {
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Really delete the selected contact?" delegate:self cancelButtonTitle:@"No, I changed my mind" destructiveButtonTitle:@"Yes, delete it" otherButtonTitles:nil];
+    [actionSheet showInView:self.view];
+    actionSheet.tag = 200;
+                                  
 }
 
 - (IBAction)showColorsActionSheet:(id)sender {
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Pick a color!" delegate:self cancelButtonTitle:nil destructiveButtonTitle:nil otherButtonTitles:@"Red", @"Orange", @"Yellow", @"Green", @"Blue", @"Indigo", @"Violet", nil];
+    [actionSheet showInView:self.view];
+    actionSheet.tag = 300;
 }
 @end
